581cc4438af2aabc02eaa7d642eea5a4fea53066
kuRajj <epideimya@gmail.com>
2022-10-29 01:37:37 +0300

    add open-feature

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,30 @@
-import {Menu} from './core/menu'
+import { Menu } from "./core/menu";
+import { getСoordinate } from "./utils"
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+  }
+  open() {
+    document.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+      this.el.style.display = "initial";
 
+      const li = document.createElement("li");
+      li.className = ".menu-item";
+      li.textContent = "AAAAAAAAAAAAAAA";
+      li.style.color = "white";
+      this.el.append(li);
+
+      const contextObj = this.el.getBoundingClientRect();
+      const bodyObj = document.body.getBoundingClientRect();
+
+      this.el.style.left =
+      getСoordinate(bodyObj.width, event.pageX, contextObj.width) + "px";
+      this.el.style.top =
+      getСoordinate(bodyObj.height, event.pageY, contextObj.height) + "px";
+    });
+  }
 }
+
+
