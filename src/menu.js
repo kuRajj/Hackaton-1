e6e97356c11be732f9f8f5bb8cbd5d78a99718f8
kuRajj <epideimya@gmail.com>
2022-10-29 02:14:38 +0300

    some changes

--- src/menu.js
+++ src/menu.js
@@ -8,11 +8,9 @@ export class ContextMenu extends Menu {
   open() {
     document.addEventListener("contextmenu", (event) => {
       event.preventDefault();
-      this.el.style.display = "initial";      
-
+      this.el.style.display = "initial";
       const contextObj = this.el.getBoundingClientRect();
       const bodyObj = document.body.getBoundingClientRect();
-
       this.el.style.left = getСoordinate(bodyObj.width, event.pageX, contextObj.width) + "px";
       this.el.style.top = getСoordinate(bodyObj.height, event.pageY, contextObj.height) + "px";
     });
