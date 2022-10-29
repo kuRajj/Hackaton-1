ac6e2545d854987c58052e46a54f2dbcf2a55183
kuRajj <epideimya@gmail.com>
2022-10-29 13:20:19 +0300

    add open menu

--- src/menu.js
+++ src/menu.js
@@ -9,10 +9,8 @@ export class ContextMenu extends Menu {
     document.addEventListener("contextmenu", (event) => {
       event.preventDefault();
       this.el.style.display = "initial";
-
       const contextObj = this.el.getBoundingClientRect();
-      const bodyObj = document.body.getBoundingClientRect();
-      
+      const bodyObj = document.body.getBoundingClientRect();      
       this.el.style.left = getСoordinate(bodyObj.width, event.pageX, contextObj.width) + "px";
       this.el.style.top = getСoordinate(bodyObj.height, event.pageY, contextObj.height) + "px";
     });
