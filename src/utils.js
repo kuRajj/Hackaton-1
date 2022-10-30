b3d990fd1dacffb123199aecb303e24124056045
Nikita Chistyakov <NikChistyakov88@yandex.ru>
2022-10-30 17:23:10 +0300

    Implemented shape module

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
 export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
+
+export function createFigureElement() {
+  const randomNumber = random(1, 9);
+  const figure = document.createElement("div");
+  figure.className = `figure figure${randomNumber}`;
+  return figure;
+}
